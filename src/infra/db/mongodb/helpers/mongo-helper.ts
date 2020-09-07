bc600cca292fac423d62b67e2b8495a689a8068c
Abner Machado <abner81@live.com>
2020-09-07 11:16:02 -0300

    feat: ensure AccountMongoRepository return an account on succes

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,9 +1,9 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
 
-  async connect(uri: string): Promise<void> {
+  async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -12,5 +12,9 @@ export const MongoHelper = {
 
   async disconnet (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
