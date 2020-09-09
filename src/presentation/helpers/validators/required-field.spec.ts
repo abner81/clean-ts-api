d26972a6493b341da873e1903db069cd99467e22
Abner Machado <abner81@live.com>
2020-09-09 11:31:03 -0300

    test: ensure RequiredField not return if validation sucess

--- src/presentation/helpers/validators/required-field.spec.ts
+++ src/presentation/helpers/validators/required-field.spec.ts
@@ -7,4 +7,10 @@ describe('Required Field Validation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('should not return if validation sucess', () => {
+    const sut = new RequiredFields('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
 })
