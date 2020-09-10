39ea5a61cfa421baba386fe7876126321a122ef7
Abner Machado <abner81@live.com>
2020-09-10 13:44:50 -0300

    refactor: move data layer interfaces to folders

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,7 +2,7 @@ import {
   AuthenticationModel,
   Authentication
 } from '../../../domain/usecases/authentication'
-import { LoadAccountByEmailRepository } from '@/data/protocols/load-account-by-email-repository'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmail: LoadAccountByEmailRepository
--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -5,7 +5,7 @@ import {
   Controller
 } from '../../presentation/protocols'
 import { serverError, ok } from '../../presentation/helpers/http/http-helper'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 
 interface SutTypes {
