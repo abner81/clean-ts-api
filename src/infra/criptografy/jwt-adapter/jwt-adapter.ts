7ee9f3a107d7ebca4fea8ef9e3085522f9766c55
Abner Machado <abner81@live.com>
2020-09-10 18:36:17 -0300

    refactor: move BcryptAdapter files a folder

---
+++ src/infra/criptografy/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+import { Encrypter } from '../../../data/protocols/criptografy/encrypter'
+
+export class JwtAdapter implements Encrypter {
+  private readonly secret: string
+  constructor(secretKey: string) {
+    this.secret = secretKey
+  }
+
+  async encrypt(value: string): Promise<string> {
+    await jwt.sign({ id: value }, this.secret)
+    return null
+  }
+}
