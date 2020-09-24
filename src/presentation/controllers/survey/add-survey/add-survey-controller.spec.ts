7c4e099cc413193ba9fa50417bb3f6f4f9da9649
Abner Machado <abner81@live.com>
2020-09-24 12:33:45 -0300

    test: ensure returns 204 on sucess

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -5,7 +5,11 @@ import {
   Validation
 } from './add-survey-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import {
+  badRequest,
+  serverError,
+  noContent
+} from '../../../helpers/http/http-helper'
 
 interface SutTypes {
   sut: AddSurveyController
@@ -78,10 +82,20 @@ describe('AddSurvey Controller', () => {
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
-   test('should return 500 if AddSurvey throws', async () => {
-     const { sut, addSurveyStub } = makeSut()
-     jest.spyOn(addSurveyStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-     const httpResponse = await sut.handle(makeFakeRequest())
-     expect(httpResponse).toEqual(serverError(new Error()))
-   })
+  test('should return 500 if AddSurvey throws', async () => {
+    const { sut, addSurveyStub } = makeSut()
+    jest
+      .spyOn(addSurveyStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('should return 200 on sucess', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
+  })
 })
