a60764b7f4acf408f7b62bb716aaf14ab9a88eeb
Abner Machado <abner81@live.com>
2020-09-30 11:41:22 -0300

    test: ensure AccountMongoRepository will not find an account if find account fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -76,38 +76,44 @@ describe('Account MongoDb Repository', () => {
     })
   })
 
-   describe('loadByToken()', () => {
-     test('should return an account on loadByToken without role', async () => {
-       const sut = makeSut()
-       await accountCollection.insertOne({
-         name: 'any_name',
-         email: 'any_email@mail.com',
-         password: 'any_password',
-         accessToken: 'any_token'
-       })
-       const account = await sut.loadByToken('any_token')
-       expect(account).toBeTruthy()
-       expect(account.id).toBeTruthy()
-       expect(account.name).toBe('any_name')
-       expect(account.email).toBe('any_email@mail.com')
-       expect(account.password).toBe('any_password')
-     })
+  describe('loadByToken()', () => {
+    test('should return an account on loadByToken without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
 
-     test('should return an account on loadByToken with role', async () => {
-       const sut = makeSut()
-       await accountCollection.insertOne({
-         name: 'any_name',
-         email: 'any_email@mail.com',
-         password: 'any_password',
-         accessToken: 'any_token',
-         role: 'any_role'
-       })
-       const account = await sut.loadByToken('any_token', 'any_role')
-       expect(account).toBeTruthy()
-       expect(account.id).toBeTruthy()
-       expect(account.name).toBe('any_name')
-       expect(account.email).toBe('any_email@mail.com')
-       expect(account.password).toBe('any_password')
-     })
-   })
+    test('should return an account on loadByToken with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+
+    test('should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
+  })
 })
