673563a0a6a0ff60092cf6e4df1cfb21a353f9e8
Abner Machado <abner81@live.com>
2020-10-05 14:49:35 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,8 @@ describe('Login Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -36,6 +41,38 @@ describe('Login Routes', () => {
           ]
         })
         .expect(403)
+    })
+
+    test('should return 204 on add survey with valid accessToken', async () => {
+       const res = await accountCollection.insertOne({
+         name: 'Abner Machado',
+         email: 'abner81@live.com',
+         password: '123',
+         role: 'admin'
+       })
+       const id = res.ops[0]._id
+       const accessToken = sign({ id }, env.jwtSecret)
+       await accountCollection.updateOne({ _id: id }, {
+         $set: {
+           accessToken
+         }
+       })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-image.com'
+            },
+            {
+              answer: 'Answer 1'
+            }
+          ]
+        })
+        .expect(204)
     })
   })
 })
